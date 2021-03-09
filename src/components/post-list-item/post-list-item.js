fe830595f926ebb5e8d156ffdc9f6f04fb0bc39d
Mike <mi5hqaaaaov@gmail.com>
2021-03-09 12:02:03 +0200

    starAndLikeOptions

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -1,28 +1,66 @@
-import React from 'react';
+import React,{ Component } from 'react';
 
-const PostListItem = () => {
-   return (
-      <li className="app-list-item d-flex justify-content-between">
-         <span className="app-list-item-label">
-            How it is going?
-         </span>
-         <div className="d-flex justify-content-center align-items-center">
-            <button
-               type="button"
-               className="btn-star btn-sn"
-            >
-               <i className="fa fa-star"></i>
-            </button>
-            <button
-               type="button"
-               className="btn-trash btn-sn"
-            >
-               <i className="fa fa-trash-o"></i>
-            </button>
-            <i className="fa fa-heart"></i>
+export default class PostListItem extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         important: false,
+         like: false
+      };
+      this.onImportant = this.onImportant.bind(this);
+      this.onLike = this.onLike.bind(this);
+   }
+
+   onImportant() {
+      this.setState(({ important }) => ({
+         important: !important
+      }));
+   }
+
+   onLike() {
+      this.setState(({ like }) => ({
+         like: !like
+      }));
+   }
+
+   render() {
+      const { label } = this.props;
+      const { important } = this.state;
+      const { like } = this.state;
+      let classNames = "app-list-item d-flex justify-content-between";
+
+      if (important == true) {
+         classNames += ' important';
+      }
+
+      if (like == true) {
+         classNames += ' like';
+      }
+
+      return (
+         <div className={classNames}>
+            <span
+               className="app-list-item-label"
+               onClick={this.onLike}>
+               {label}
+            </span>
+            <div className="d-flex justify-content-center align-items-center">
+               <button
+                  type="button"
+                  className="btn-star btn-sn"
+                  onClick={this.onImportant}
+               >
+                  <i className="fa fa-star"></i>
+               </button>
+               <button
+                  type="button"
+                  className="btn-trash btn-sn"
+               >
+                  <i className="fa fa-trash-o"></i>
+               </button>
+               <i className="fa fa-heart"></i>
+            </div>
          </div>
-      </li>
-   );
-};
-
-export default PostListItem;
+      );
+   }
+}
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -2,12 +2,30 @@ import React from 'react';
 
 import PostListItem from '../post-list-item';
 
-const PostList = () => {
+const PostList = ({ posts }) => {
+
+   const elements = posts.map((item) => {
+      // Простой способ проверки на объект + содержится ли в нем информация
+      if (typeof item === 'object' && isEmpty(item)) {
+         const { id,...itemProps } = item;
+         return (
+            <li key={id} className='list-group-item'>
+               <PostListItem {...itemProps} />
+            </li>
+         );
+      }
+   });
+
+   function isEmpty(obj) {
+      for (let key in obj) {
+         return true;
+      }
+      return false;
+   }
+
    return (
       <ul className="app-list list-group">
-         <PostListItem />
-         <PostListItem />
-         <PostListItem />
+         {elements}
       </ul>
    );
 };
